40f0faa03d50bc26f27f0859a72e47276aee8955
Mario andre <mario.andre.franco@gmail.com>
2021-09-30 11:24:05 -0300

    refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = ({ black }) => {
   return (
-    <header className="black">
+    <header className={black}>
       <div className="header--logo">
         <a href="/">
-          <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png"></img>
+          <img
+            alt="logo"
+            src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png"
+          ></img>
         </a>
       </div>
       <div className="header--user">
         <a href="/">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"></img>
+          <img
+            alt="user"
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          ></img>
         </a>
       </div>
     </header>
